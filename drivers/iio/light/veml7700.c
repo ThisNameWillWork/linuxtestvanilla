60dee09908dee7e022bd0da9a7afcb15020f450f
Unknown <a.kilian@protelematik.de>
2017-10-25 14:38:30 +0200

    veml7700

--- drivers/iio/light/veml7700.c
+++ drivers/iio/light/veml7700.c
@@ -1,38 +1,89 @@
-#include <linux/init.h>
+/*
+ * veml7700.c - Support for Vishay veml7700 combined ambient light and
+ * proximity sensor
+ *
+ * Copyright 2012 Peter Meerwald <pmeerw@pmeerw.net>
+ *
+ * This file is subject to the terms and conditions of version 2 of
+ * the GNU General Public License.  See the file COPYING in the main
+ * directory of this archive for more details.
+ *
+ * IIO driver for veml7700 (7-bit I2C slave address 0x13)
+ *
+ * TODO:
+ *   allow to adjust IR current
+ *   proximity threshold and event handling
+ */
+
 #include <linux/module.h>
+#include <linux/i2c.h>
+#include <linux/err.h>
+#include <linux/delay.h>
 
-static int my_init(void)
-{
- return  0;
-}
+#include <linux/iio/iio.h>
+#include <linux/iio/sysfs.h>
 
-static void my_exit(void)
-{
- return;
-}
+#define veml7700_DRV_NAME "veml7700"
 
-static struct veml7700 my_match_table[] = {
-     {
-             .compatible = "veml77000",
-     },
-     {0}
-};
-MODULE_DEVICE_TABLE(of, my_match_table);
-
-static struct platform_driver my_platform_driver = {
-        .probe = my_probe,
-        .remove = my_remove,
-        .driver = {
-                .name = "veml7700",
-                .owner = THIS_MODULE,
-                .of_match_table = of_match_ptr(my_match_table),
-        },
+struct veml7700_data {
+	struct i2c_client *client;
 };
 
-static int my_probe(struct platform_device *dev)
+static const struct i2c_device_id veml7700_id[] = {
+	{ "veml7700", 0 },
+	{ }
+};
+MODULE_DEVICE_TABLE(i2c, veml7700_id);
+
+static const struct iio_chan_spec veml7700_channels[] = {
+	{
+		.type = IIO_LIGHT,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW) |
+			BIT(IIO_CHAN_INFO_SCALE),
+	}, {
+		.type = IIO_PROXIMITY,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+	}
+};
+
+static int veml7700_read_raw(struct iio_dev *indio_dev,
+				struct iio_chan_spec const *chan,
+				int *val, int *val2, long mask)
 {
- return 0;
+
+	return 0;
 }
 
-module_init(my_init);
-module_exit(my_exit);
+static const struct iio_info veml7700_info = {
+	.read_raw = veml7700_read_raw,
+	.driver_module = THIS_MODULE,
+};
+
+static int veml7700_probe(struct i2c_client *client,
+			  const struct i2c_device_id *id)
+{
+	printk("######################################### PROBE\n");
+	return 0;
+}
+
+static int veml7700_remove(struct i2c_client *client)
+{
+	printk("######################################### REMOVE\n");
+	return 0;
+}
+
+static struct i2c_driver veml7700_driver = {
+	.driver = {
+		.name   = veml7700_DRV_NAME,
+		.owner  = THIS_MODULE,
+	},
+	.probe  = veml7700_probe,
+	.remove = veml7700_remove,
+	.id_table = veml7700_id,
+};
+
+module_i2c_driver(veml7700_driver);
+
+MODULE_AUTHOR("AK");
+MODULE_DESCRIPTION("Vishay veml7700 ambient light sensor driver");
+MODULE_LICENSE("GPL");
