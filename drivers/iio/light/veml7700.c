fb792b0ac876ae453df2293c966fa095c30168fe
Unknown <a.kilian@protelematik.de>
2017-11-03 16:11:15 +0100

    veml7700

--- drivers/iio/light/veml7700.c
+++ drivers/iio/light/veml7700.c
@@ -23,7 +23,7 @@ Description    :     LINUX DEVICE DRIVER PROJECT
 #define veml7700_DRV_NAME "veml7700"
 #define DEBUG 1
 
-#define COMMAND_ALS_SM      0x00   /**/ 
+#define COMMAND_ALS_SM 		 0x00   /**/ 
 #define COMMAND_ALS_WH      0x01   /**/ 
 #define COMMAND_ALS_WL      0x02   /**/ 
 #define COMMAND_PSM         0x03   /**/ 
@@ -69,23 +69,23 @@ Description    :     LINUX DEVICE DRIVER PROJECT
 
   enum { STATUS_OK = 0, STATUS_ERROR = 0xff };
 
-  //########################################################################
+//   //########################################################################
 
-  enum { I2C_ADDRESS = 0x10 };
-  enum { COMMAND_ALS_SM = 0x00, ALS_SM_MASK = 0x1800, ALS_SM_SHIFT = 11 };
-  enum { COMMAND_ALS_IT = 0x00, ALS_IT_MASK = 0x03c0, ALS_IT_SHIFT = 6 };
-  enum { COMMAND_ALS_PERS = 0x00, ALS_PERS_MASK = 0x0030, ALS_PERS_SHIFT = 4 };
-  enum { COMMAND_ALS_INT_EN = 0x00, ALS_INT_EN_MASK = 0x0002,
-         ALS_INT_EN_SHIFT = 1 };
-  enum { COMMAND_ALS_SD = 0x00,  ALS_SD_MASK = 0x0001, ALS_SD_SHIFT = 0 };
-  enum { COMMAND_ALS_WH = 0x01 };
-  enum { COMMAND_ALS_WL = 0x02 };
-  enum { COMMAND_PSM = 0x03, PSM_MASK = 0x0006, PSM_SHIFT = 1 };
-  enum { COMMAND_PSM_EN = 0x03, PSM_EN_MASK = 0x0001, PSM_EN_SHIFT = 0 };
-  enum { COMMAND_ALS = 0x04 };
-  enum { COMMAND_WHITE = 0x05 };
-  enum { COMMAND_ALS_IF_L = 0x06, ALS_IF_L_MASK = 0x8000, ALS_IF_L_SHIFT = 15 };
-enum { COMMAND_ALS_IF_H = 0x06, ALS_IF_H_MASK = 0x4000, ALS_IF_H_SHIFT = 14 };
+//   enum { I2C_ADDRESS = 0x10 };
+//   enum { COMMAND_ALS_SM = 0x00, ALS_SM_MASK = 0x1800, ALS_SM_SHIFT = 11 };
+//   enum { COMMAND_ALS_IT = 0x00, ALS_IT_MASK = 0x03c0, ALS_IT_SHIFT = 6 };
+//   enum { COMMAND_ALS_PERS = 0x00, ALS_PERS_MASK = 0x0030, ALS_PERS_SHIFT = 4 };
+//   enum { COMMAND_ALS_INT_EN = 0x00, ALS_INT_EN_MASK = 0x0002,
+//          ALS_INT_EN_SHIFT = 1 };
+//   enum { COMMAND_ALS_SD = 0x00,  ALS_SD_MASK = 0x0001, ALS_SD_SHIFT = 0 };
+//   enum { COMMAND_ALS_WH = 0x01 };
+//   enum { COMMAND_ALS_WL = 0x02 };
+//   enum { COMMAND_PSM = 0x03, PSM_MASK = 0x0006, PSM_SHIFT = 1 };
+//   enum { COMMAND_PSM_EN = 0x03, PSM_EN_MASK = 0x0001, PSM_EN_SHIFT = 0 };
+//   enum { COMMAND_ALS = 0x04 };
+//   enum { COMMAND_WHITE = 0x05 };
+//   enum { COMMAND_ALS_IF_L = 0x06, ALS_IF_L_MASK = 0x8000, ALS_IF_L_SHIFT = 15 };
+// enum { COMMAND_ALS_IF_H = 0x06, ALS_IF_H_MASK = 0x4000, ALS_IF_H_SHIFT = 14 };
 
 
 
@@ -316,26 +316,26 @@ static int __init veml7700_init(void)
 	}
 
 
-  // write initial state to VEML7700
-  register_cache[0] = ( (uint16_t(ALS_GAIN_x2) << ALS_SM_SHIFT) |
-                        (uint16_t(ALS_INTEGRATION_100ms) << ALS_IT_SHIFT) |
-                        (uint16_t(ALS_PERSISTENCE_1) << ALS_PERS_SHIFT) |
-                        (uint16_t(0) << ALS_INT_EN_SHIFT) |
-                        (uint16_t(0) << ALS_SD_SHIFT) );
-  register_cache[1] = 0x0000;
-  register_cache[2] = 0xffff;
-  register_cache[3] = ( (uint16_t(ALS_POWER_MODE_3) << PSM_SHIFT) | (uint16_t(0) << PSM_EN_SHIFT) );
+  // // write initial state to VEML7700
+  // register_cache[0] = ( (uint16_t(ALS_GAIN_x2) << ALS_SM_SHIFT) |
+  //                       (uint16_t(ALS_INTEGRATION_100ms) << ALS_IT_SHIFT) |
+  //                       (uint16_t(ALS_PERSISTENCE_1) << ALS_PERS_SHIFT) |
+  //                       (uint16_t(0) << ALS_INT_EN_SHIFT) |
+  //                       (uint16_t(0) << ALS_SD_SHIFT) );
+  // register_cache[1] = 0x0000;
+  // register_cache[2] = 0xffff;
+  // register_cache[3] = ( (uint16_t(ALS_POWER_MODE_3) << PSM_SHIFT) | (uint16_t(0) << PSM_EN_SHIFT) );
 
-  int x = 99;
+  // int x = 99;
 
-  x = i2c_smbus_write_byte_data(priv->client, 0x00, register_cache[0]);
-  printk(KERN_DEBUG "VEML7700 ######################################### CONF0: %d\n",x);
-  x = i2c_smbus_write_byte_data(priv->client, 0x01, register_cache[1]);
-  printk(KERN_DEBUG "VEML7700 ######################################### CONF1: %d\n",x);
-  x = i2c_smbus_write_byte_data(priv->client, 0x02, register_cache[2]);
-  printk(KERN_DEBUG "VEML7700 ######################################### CONF2: %d\n",x);
-  x = i2c_smbus_write_byte_data(priv->client, 0x03, register_cache[3]);
-  printk(KERN_DEBUG "VEML7700 ######################################### CONF3: %d\n",x);
+  // x = i2c_smbus_write_byte_data(priv->client, 0x00, register_cache[0]);
+  // printk(KERN_DEBUG "VEML7700 ######################################### CONF0: %d\n",x);
+  // x = i2c_smbus_write_byte_data(priv->client, 0x01, register_cache[1]);
+  // printk(KERN_DEBUG "VEML7700 ######################################### CONF1: %d\n",x);
+  // x = i2c_smbus_write_byte_data(priv->client, 0x02, register_cache[2]);
+  // printk(KERN_DEBUG "VEML7700 ######################################### CONF2: %d\n",x);
+  // x = i2c_smbus_write_byte_data(priv->client, 0x03, register_cache[3]);
+  // printk(KERN_DEBUG "VEML7700 ######################################### CONF3: %d\n",x);
 
 
 	PINFO("INIT\n");
