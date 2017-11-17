05d86acac01354eb47abc68b170b8fd2a02d8607
Unknown <a.kilian@protelematik.de>
2017-11-17 10:36:03 +0100

    M24SR Test

--- drivers/nfc/st21nfca/i2c.c
+++ drivers/nfc/st21nfca/i2c.c
@@ -112,10 +112,7 @@ static int st21nfca_hci_platform_init(struct st21nfca_i2c_phy *phy)
 {
 	u16 wait_reboot[] = { 50, 300, 1000 };
 	char reboot_cmd[] = { 0x7E, 0x66, 0x48, 0xF6, 0x7E };
-	u8 tmp[ST21NFCA_HCI_LLC_MAX_SIZE];
-	int i, r = -1;
-
-	for (i = 0; i < ARRAY_SIZE(wait_reboot) && r < 0; i++) {
+	u+) {
 		r = i2c_master_send(phy->i2c_dev, reboot_cmd,
 				    sizeof(reboot_cmd));
 		if (r < 0)
