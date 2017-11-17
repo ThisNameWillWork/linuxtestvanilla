5cda5110fe2fbcaa38d741535e4fd926c9f13ea8
Unknown <a.kilian@protelematik.de>
2017-11-17 11:21:49 +0100

    M24SR

--- drivers/nfc/m24sr.h
+++ drivers/nfc/m24sr.h
@@ -29,6 +29,16 @@
 #define PDEBUG(fmt,args...) printk(KERN_DEBUG"%s:"fmt,DRIVER_NAME, ##args)
 #define PERR(fmt,args...) printk(KERN_ERR"%s:"fmt,DRIVER_NAME,##args)
 #define PINFO(fmt,args...) printk(KERN_INFO"%s:"fmt,DRIVER_NAME, ##args)
+#include<linux/cdev.h>
+#include<linux/device.h>
+#include<linux/fs.h>
+#include<linux/i2c.h>
+#include<linux/init.h>
+#include<linux/kdev_t.h>
+#include<linux/module.h>
+#include<linux/slab.h>
+#include<linux/types.h>
+#include<linux/uaccess.h>
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __DRV_M24SR_H
